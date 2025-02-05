e48bbecbf62085278b4e99d49afdb4923376a883
Cameron Prince <cameron@teslauniverse.com>
2025-02-05 14:32:06 -0600

    Sets up sub-classing for output & provides PIO driver.

---
+++ hardware/output/output.py
@@ -0,0 +1,14 @@
+"""
+MicroPython Tesla Coil Controller (MPTCC)
+by Cameron Prince
+teslauniverse.com
+
+hardware/output/output.py
+Parent class for output devices.
+"""
+
+from ..hardware import Hardware
+
+class Output(Hardware):
+    def __init__(self):
+        super().__init__()
